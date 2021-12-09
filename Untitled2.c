eb37cb99ce12427e21a9d9e7bd9e22a99c3201f8
Domi123789 <89708991+Domi123789@users.noreply.github.com>
2021-12-09 08:24:29 +0100

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{
+    int stranicaA = 5;
+    int stranicaB = 10;
+    int stranicaC = 7;
+    int mnozenje = stranicaA * stranicaB * stranicaC;
+    printf("%d", mnozenje);
+}
