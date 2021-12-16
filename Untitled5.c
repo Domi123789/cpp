03a5000fb64b93f580aee3816dbea2623113492c
Domi123789 <89708991+Domi123789@users.noreply.github.com>
2021-12-16 09:26:57 +0100

    Add files via upload

---
+++ Untitled5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+
+   printf("povrsinaTrokuta = %d\n", (a*b)/2);
+    return 0;
+}
