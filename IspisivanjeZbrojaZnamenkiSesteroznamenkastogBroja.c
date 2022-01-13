133852b072f088a055dc372a46d7f39c1c612124
Domi123789 <89708991+Domi123789@users.noreply.github.com>
2022-01-13 10:18:28 +0100

    Update IspisivanjeZbrojaZnamenkiSesteroznamenkastogBroja.c

--- IspisivanjeZbrojaZnamenkiSesteroznamenkastogBroja.c
+++ IspisivanjeZbrojaZnamenkiSesteroznamenkastogBroja.c
@@ -5,7 +5,7 @@ int main()
 {
     int a;
     printf ("Unesi sesteroznamenkasti broj:\n");
-    scanf ("%d", a);
+    scanf ("%d", &a);
     int zbrajanje = a%10+(a/10)%10+(a/100)%10+(a/1000)%10+(a/10000)%10+(a/100000)%10+(a/1000000);
     printf ("%d", zbrajanje);
 
