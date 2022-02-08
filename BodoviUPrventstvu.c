30be169c8d8784da53c9c3e65522529aaa0c7b87
Domi123789 <89708991+Domi123789@users.noreply.github.com>
2022-02-08 21:06:40 +0100

    Add files via upload

---
+++ BodoviUPrventstvu.c
@@ -0,0 +1,220 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    int x1;
+    int x2;
+    int x3;
+    int x4;
+    int x5;
+    int x6;
+    int x7;
+    int x8;
+    int x9;
+    int x10;
+    int y1;
+    int y2;
+    int y3;
+    int y4;
+    int y5;
+    int y6;
+    int y7;
+    int y8;
+    int y9;
+    int y10;
+
+    printf ("Unesite broj zabijenih golova prve momcadi u prvom kolu\n");
+    scanf ("%d", &x1);
+    printf ("Unesite broj zabijenih golova druge momcadi u prvom kolu\n");
+    scanf ("%d", &y1);
+
+
+    if (x1 > y1)
+    {
+        printf ("Prva momcad je osvojila 3 boda, a druga 0\n");
+    }
+    else if (x1 < y1)
+    {
+        printf ("Druga momcad je osvojila 3 boda, a prva 0\n");
+    }
+    else
+    {
+        printf ("Obadvije momcadi su postigle jednak broj golova, te svaka momcad dobiva 1 bod\n");
+    }
+
+
+    printf ("Unesite broj zabijenih golova prve momcadi u drugom kolu\n");
+    scanf ("%d", &x2);
+    printf ("Unesite broj zabijenih golova druge momcadi u drugom kolu\n");
+    scanf ("%d", &y2);
+
+    if (x2 > y2)
+    {
+        printf ("Prva momcad je osvojila 3 boda, a druga 0\n");
+    }
+    else if (x2 < y2)
+    {
+        printf ("Druga momcad je osvojila 3 boda, a prva 0\n");
+    }
+    else
+    {
+        printf ("Obadvije momcadi su postigle jednak broj golova, te svaka momcad dobiva 1 bod\n");
+    }
+
+    printf ("Unesite broj zabijenih golova prve momcadi u trecem kolu\n");
+    scanf ("%d", &x3);
+    printf ("Unesite broj zabijenih golova druge momcadi u trecem kolu\n");
+    scanf ("%d", &y3);
+
+    if (x3 > y3)
+    {
+        printf ("Prva momcad je osvojila 3 boda, a druga 0\n");
+    }
+    else if (x3 < y3)
+    {
+        printf ("Druga momcad je osvojila 3 boda, a prva 0\n");
+    }
+    else
+    {
+        printf ("Obadvije momcadi su postigle jednak broj golova, te svaka momcad dobiva 1 bod\n");
+    }
+
+    printf ("Unesite broj zabijenih golova prve momcadi u cetvrtom kolu\n");
+    scanf ("%d", &x4);
+    printf ("Unesite broj zabijenih golova druge momcadi u cetvrtom kolu\n");
+    scanf ("%d", &y4);
+
+    if (x4 > y4)
+    {
+        printf ("Prva momcad je osvojila 3 boda, a druga 0\n");
+    }
+    else if (x4 < y4)
+    {
+        printf ("Druga momcad je osvojila 3 boda, a prva 0\n");
+    }
+    else
+    {
+        printf ("Obadvije momcadi su postigle jednak broj golova, te svaka momcad dobiva 1 bod\n");
+    }
+
+    printf ("Unesite broj zabijenih golova prve momcadi u petom kolu\n");
+    scanf ("%d", &x5);
+    printf ("Unesite broj zabijenih golova druge momcadi u petom kolu\n");
+    scanf ("%d", &y5);
+
+    if (x5 > y5)
+    {
+        printf ("Prva momcad je osvojila 3 boda, a druga 0\n");
+    }
+    else if (x5 < y5)
+    {
+        printf ("Druga momcad je osvojila 3 boda, a prva 0\n");
+    }
+    else
+    {
+        printf ("Obadvije momcadi su postigle jednak broj golova, te svaka momcad dobiva 1 bod\n");
+    }
+
+    printf ("Unesite broj zabijenih golova prve momcadi u sestom kolu\n");
+    scanf ("%d", &x6);
+    printf ("Unesite broj zabijenih golova druge momcadi u sestom kolu\n");
+    scanf ("%d", &y6);
+
+    if (x6 > y6)
+    {
+        printf ("Prva momcad je osvojila 3 boda, a druga 0\n");
+    }
+    else if (x6 < y6)
+    {
+        printf ("Druga momcad je osvojila 3 boda, a prva 0\n");
+    }
+    else
+    {
+        printf ("Obadvije momcadi su postigle jednak broj golova, te svaka momcad dobiva 1 bod\n");
+    }
+
+    printf ("Unesite broj zabijenih golova prve momcadi u sedmom kolu\n");
+    scanf ("%d", &x7);
+    printf ("Unesite broj zabijenih golova druge momcadi u sedmom kolu\n");
+    scanf ("%d", &y7);
+
+    if (x7 > y7)
+    {
+        printf ("Prva momcad je osvojila 3 boda, a druga 0\n");
+    }
+    else if (x7 < y7)
+    {
+        printf ("Druga momcad je osvojila 3 boda, a prva 0\n");
+    }
+    else
+    {
+        printf ("Obadvije momcadi su postigle jednak broj golova, te svaka momcad dobiva 1 bod\n");
+    }
+
+    printf ("Unesite broj zabijenih golova prve momcadi u osmom kolu\n");
+    scanf ("%d", &x8);
+    printf ("Unesite broj zabijenih golova druge momcadi u osmom kolu\n");
+    scanf ("%d", &y8);
+
+
+    if (x8 > y8)
+    {
+        printf ("Prva momcad je osvojila 3 boda, a druga 0\n");
+    }
+    else if (x8 < y8)
+    {
+        printf ("Druga momcad je osvojila 3 boda, a prva 0\n");
+    }
+    else
+    {
+        printf ("Obadvije momcadi su postigle jednak broj golova, te svaka momcad dobiva 1 bod\n");
+    }
+
+    printf ("Unesite broj zabijenih golova prve momcadi u devetom kolu\n");
+    scanf ("%d", &x9);
+    printf ("Unesite broj zabijenih golova druge momcadi u devetom kolu\n");
+    scanf ("%d", &y9);
+
+    if (x9 > y9)
+    {
+        printf ("Prva momcad je osvojila 3 boda, a druga 0\n");
+    }
+    else if (x9 < y9)
+    {
+        printf ("Druga momcad je osvojila 3 boda, a prva 0\n");
+    }
+    else
+    {
+        printf ("Obadvije momcadi su postigle jednak broj golova, te svaka momcad dobiva 1 bod\n");
+    }
+
+    printf ("Unesite broj zabijenih golova prve momcadi u desetom kolu\n");
+    scanf ("%d", &x10);
+    printf ("Unesite broj zabijenih golova druge momcadi u desetom kolu\n");
+    scanf ("%d", &y10);
+
+    if (x10 > y10)
+    {
+        printf ("Prva momcad je osvojila 3 boda, a druga 0\n");
+    }
+    else if (x10 < y10)
+    {
+        printf ("Druga momcad je osvojila 3 boda, a prva 0\n");
+    }
+    else
+    {
+        printf ("Obadvije momcadi su postigle jednak broj golova, te svaka momcad dobiva 1 bod\n\n");
+    }
+
+    int zbrojGolovaPrveMomcadi = x1+x2+x3+x4+x5+x6+x7+x8+x9+x10;
+    printf ("Prva momcad je zabila %d golova\n", zbrojGolovaPrveMomcadi);
+
+    int zbrojGolovaDrugeMomcadi = y1+y2+y3+y4+y5+y6+y7+y8+y9+y10;
+    printf ("Druga momcad je zabila %d golova\n", zbrojGolovaDrugeMomcadi);
+
+    int ukupanZbrojGolovaObadvijeMomcadi = zbrojGolovaPrveMomcadi + zbrojGolovaDrugeMomcadi;
+    printf ("Dvije momcadi su zajedno zabili %d golova", ukupanZbrojGolovaObadvijeMomcadi);
+
+    return 0;
+}
