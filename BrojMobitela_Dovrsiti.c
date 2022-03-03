a7f1cab3b480a1630b532f4a557661dcf8c137e9
Domi123789 <89708991+Domi123789@users.noreply.github.com>
2022-03-03 10:23:04 +0100

    Add files via upload

---
+++ BrojMobitela_Dovrsiti.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int cc;
+    int abc; //2
+    int def; //3
+    int ghi; //4
+    int jkl; //5
+    int mno; //6
+    int pqrs; //7
+    int tuv; //8
+    int wxyz; //9
+
+    switch (cc)
+    {
+    case 2:
+        printf ("Unesite broj");
+        scanf ("%d",&abc);
+        switch(abc)
+        {
+            case 1:
+        printf("A");
+        }
+
+        {
+            case 2:
+        printf ("B");
+        }
+        {
+            case 3:
+        printf ("C");
+        }
+    case 3:
+    printf("Unesi broj");
+    scanf ("d", &def);
+
+    }
+}
