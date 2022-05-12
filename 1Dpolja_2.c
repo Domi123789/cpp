1080eba2667826fb86282076a1ea1f4dee4cf042
Domi123789 <89708991+Domi123789@users.noreply.github.com>
2022-05-12 08:39:46 +0200

    Add files via upload

---
+++ 1Dpolja_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    int a[5]={16,-3,4,2,10};
+
+
+    for (i=4; i>=0; i--)
+    {
+        printf ("%d\n", a[i]);
+    }
+
+    return 0;
+}
